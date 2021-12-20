7731590379a31045fb6c24ed7396813a60beaf88
Jason Sengmany <jasonsengmany@outlook.com>
2021-12-20 18:32:46 +1100

    Removed duplicated project files

--- Services/PersistenceServices/IPersistenceService.cs
+++
@@ -1,9 +0,0 @@
-namespace AudioAnalyzer.Services;
-
-public interface IPersistenceService
-{
-    Task Save(List<Song> songs, string path);
-
-    Task Append(List<Song> songs, string path);
-    Task<List<Song>> Load(string path);
-}
--- _Usings.cs
+++
@@ -1,2 +0,0 @@
-global using AudioAnalyzer.Models;
-global using System.Diagnostics;
